85bd71244421f867a89921d118a8454cbc3a4a37
userkate <116511308+userkate@users.noreply.github.com>
2022-12-12 22:09:35 +0300

    Update CreditPaymentService.java

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,8 +1,8 @@
 public class CreditPaymentService {
     public double calculate(int credit, int time, double percent) {
-        double creditrate = percent / 12 / 100;     // ставка в месяц
-        double degree = Math.pow(1 + creditrate, time); // степень
-        double annuity = creditrate * degree / (degree-1);        // аннуитет
+        double creditrate = percent / 12 / 100;                // ставка в месяц
+        double degree = Math.pow(1 + creditrate, time);       // степень
+        double annuity = creditrate * degree / (degree-1);   // аннуитет
         double payment = annuity * credit;
         return payment;
     }
